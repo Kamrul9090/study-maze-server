587b10707bf5e6bd7e807e3f0976c6e72d6c0de8
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-05-21 22:17:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+
+// USER_NAME = study-maze
+// PASSWORD = amar-study-maze
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.06w34xu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const courses = client.db('study-maze').collection('courses');
+        const categories = client.db('study-maze').collection('categories');
+
+        app.get('/courses', async (req, res) => {
+            const result = await courses.find({}).toArray();
+            res.send(result);
+        })
+        app.get('/categories', async (req, res) => {
+            const result = await categories.find({}).toArray();
+            res.send(result);
+        })
+        app.get('/courses/:id', async (req, res) => {
+            const param = req.params.id;
+            console.log(param);
+            const result = await courses.findOne({ _id: new ObjectId(param) });
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
+
+app.get('/', async (req, res) => {
+    res.send('Study Maze server is running');
+})
+
+app.listen(port, () => console.log(`Study Maze running on ${port}`))
+
+
